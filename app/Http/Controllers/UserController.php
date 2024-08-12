9ac01c6d0ab3234aa73445aedebb71de864b117f
zwsaqx <77838223+zwsaqx@users.noreply.github.com>
2024-08-12 16:46:48 +1200

    fix: refactor

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,41 +9,43 @@ use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
-    public function register(Request $request){
-$incomingFields=$request->validate([
-    'name'=>['required','min:3','max:100',Rule::unique('users','name')],
-    'email'=>['required',Rule::unique('users','email')],
-    'password'=>['required','min:8','max:25']
-]);
+    public function register(Request $request)
+    {
+        $incomingFields = $request->validate([
+            'name' => ['required', 'min:3', 'max:100', Rule::unique('users', 'name')],
+            'email' => ['required', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:8', 'max:25']
+        ]);
 
-    $incomingFields['password']=bcrypt($incomingFields['password']);
-    $user=User::create($incomingFields);
-    auth()->login($user);
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+        $user = User::create($incomingFields);
+        auth()->login($user);
         return redirect('/Home');
     }
 
-    public function login(Request $request){
-        $incomingFields=$request->validate([
-            'loginName'=>'required',
-            'loginPassword'=>'required'
+    public function login(Request $request)
+    {
+        $incomingFields = $request->validate([
+            'loginName' => 'required',
+            'loginPassword' => 'required'
         ]);
 
-        //Check if loginName and loginPassword matches name and password from sql DB 
-        if(auth()->attempt(['name'=>$incomingFields['loginName'],'password'=>$incomingFields['loginPassword']])){
+        //Check if loginName and loginPassword matches name and password from sql DB
+        if (auth()->attempt(['name' => $incomingFields['loginName'], 'password' => $incomingFields['loginPassword']])) {
             $request->session()->regenerate();
             return redirect("/Home");
-        }
-        else{
+        } else {
             return redirect()->back()->with('error', 'Invalid username or password. Please try again.');
-            
+
         }
-        
-      
-        
-        
+
+
+
+
     }
 
-    public function logout(){
+    public function logout()
+    {
         auth()->logout();
         return redirect('/');
     }
