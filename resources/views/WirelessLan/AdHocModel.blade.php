35e6f9de802b6692c59d6fbe5a341b2e9555c9ea
Mark Soluiman <markashraf1030@gmail.com>
2024-08-24 15:41:44 +1200

    WirelessModelling finished

--- resources/views/WirelessLan/AdHocModel.blade.php
+++ resources/views/WirelessLan/AdHocModel.blade.php
@@ -229,3 +229,319 @@
                 @endif
             </td>
         </tr>
+        <tr>
+            <td width="60">{!! $deviceLayout[6] !!}</td>
+            <td width="60" align="center">
+                @if ($totalDevices >= 8)
+                    <img src="{{ asset('Images/Wireless/comm_horiz.gif') }}">
+                @else
+                @endif
+            </td>
+            <td width="60">{!! $deviceLayout[7] !!}</td>
+            <td width="60" align="center">
+                @if ($totalDevices >= 9)
+                    <img src="{{ asset('Images/Wireless/comm_horiz.gif') }}">
+                @else
+                @endif
+            </td>
+            <td width="60">{!! $deviceLayout[8] !!}</td>
+            <td width="60" align="center">
+                @if ($totalDevices >= 15)
+                    <img src="{{ asset('Images/Wireless/comm_horiz.gif') }}">
+                @else
+                @endif
+            </td>
+            <td width="60">{!! $deviceLayout[14] !!}</td>
+            <td width="60" align="center">
+                @if ($totalDevices >= 23)
+                    <img src="{{ asset('Images/Wireless/comm_horiz.gif') }}">
+                @else
+                @endif
+            </td>
+            <td width="60">{!! $deviceLayout[22] !!}</td>
+        </tr>
+        <tr>
+            <td align="center">
+                @if ($totalDevices >= 10)
+                    <img src="{{ asset('Images/Wireless/comm_vert.gif') }}">
+                @else
+                @endif
+            </td>
+            <td>
+                @if ($totalDevices >= 11)
+                    <img src="{{ asset('Images/Wireless/comm_slash2.gif') }}">
+                @elseif ($totalDevices >= 10)
+                    <img src="{{ asset('Images/Wireless/comm_slash1.gif') }}">
+                @else
+                @endif
+            </td>
+            <td align="center">
+                @if ($totalDevices >= 11)
+                    <img src="{{ asset('Images/Wireless/comm_vert.gif') }}">
+                @else
+                @endif
+            </td>
+            <td>
+                @if ($totalDevices >= 12)
+                    <img src="{{ asset('Images/Wireless/comm_slash2.gif') }}">
+                @elseif ($totalDevices >= 11)
+                    <img src="{{ asset('Images/Wireless/comm_slash1.gif') }}">
+                @else
+                @endif
+            </td>
+            <td align="center">
+                @if ($totalDevices >= 12)
+                    <img src="{{ asset('Images/Wireless/comm_vert.gif') }}">
+                @else
+                @endif
+            </td>
+            <td>
+                @if ($totalDevices >= 16)
+                    <img src="{{ asset('Images/Wireless/comm_slash2.gif') }}">
+                @elseif ($totalDevices >= 15)
+                    <img src="{{ asset('Images/Wireless/comm_slash1.gif') }}">
+                @else
+                @endif
+            </td>
+            <td align="center">
+                @if ($totalDevices >= 16)
+                    <img src="{{ asset('Images/Wireless/comm_vert.gif') }}">
+                @else
+                @endif
+            </td>
+            <td>
+                @if ($totalDevices >= 24)
+                    <img src="{{ asset('Images/Wireless/comm_slash2.gif') }}">
+                @elseif ($totalDevices >= 23)
+                    <img src="{{ asset('Images/Wireless/comm_slash1.gif') }}">
+                @else
+                @endif
+            </td>
+            <td align="center">
+                @if ($totalDevices >= 24)
+                    <img src="{{ asset('Images/Wireless/comm_vert.gif') }}">
+                @else
+                @endif
+            </td>
+        </tr>
+        <tr>
+            <td width="60">{!! $deviceLayout[9] !!}</td>
+            <td width="60" align="center">
+                @if ($totalDevices >= 11)
+                    <img src="{{ asset('Images/Wireless/comm_horiz.gif') }}">
+                @else
+                @endif
+            </td>
+            <td width="60">{!! $deviceLayout[10] !!}</td>
+            <td width="60" align="center">
+                @if ($totalDevices >= 12)
+                    <img src="{{ asset('Images/Wireless/comm_horiz.gif') }}">
+                @else
+                @endif
+            </td>
+            <td width="60">{!! $deviceLayout[11] !!}</td>
+            <td width="60" align="center">
+                @if ($totalDevices >= 16)
+                    <img src="{{ asset('Images/Wireless/comm_horiz.gif') }}">
+                @else
+                @endif
+            </td>
+            <td width="60">{!! $deviceLayout[15] !!}</td>
+            <td width="60" align="center">
+                @if ($totalDevices >= 24)
+                    <img src="{{ asset('Images/Wireless/comm_horiz.gif') }}">
+                @else
+                @endif
+            </td>
+            <td width="60">{!! $deviceLayout[23] !!}</td>
+        </tr>
+        <tr>
+            <td align="center">
+                @if ($totalDevices >= 17)
+                    <img src="{{ asset('Images/Wireless/comm_vert.gif') }}">
+                @else
+                @endif
+            </td>
+            <td>
+                @if ($totalDevices >= 18)
+                    <img src="{{ asset('Images/Wireless/comm_slash2.gif') }}">
+                @elseif ($totalDevices >= 17)
+                    <img src="{{ asset('Images/Wireless/comm_slash1.gif') }}">
+                @else
+                @endif
+            </td>
+            <td align="center">
+                @if ($totalDevices >= 18)
+                    <img src="{{ asset('Images/Wireless/comm_vert.gif') }}">
+                @else
+                @endif
+            </td>
+            <td>
+                @if ($totalDevices >= 19)
+                    <img src="{{ asset('Images/Wireless/comm_slash2.gif') }}">
+                @elseif ($totalDevices >= 18)
+                    <img src="{{ asset('Images/Wireless/comm_slash1.gif') }}">
+                @else
+                @endif
+            </td>
+            <td align="center">
+                @if ($totalDevices >= 19)
+                    <img src="{{ asset('Images/Wireless/comm_vert.gif') }}">
+                @else
+                @endif
+            </td>
+            <td>
+                @if ($totalDevices >= 20)
+                    <img src="{{ asset('Images/Wireless/comm_slash2.gif') }}">
+                @elseif ($totalDevices >= 19)
+                    <img src="{{ asset('Images/Wireless/comm_slash1.gif') }}">
+                @else
+                @endif
+            </td>
+            <td align="center">
+                @if ($totalDevices >= 20)
+                    <img src="{{ asset('Images/Wireless/comm_vert.gif') }}">
+                @else
+                @endif
+            </td>
+            <td>
+                @if ($totalDevices >= 25)
+                    <img src="{{ asset('Images/Wireless/comm_slash2.gif') }}">
+                @elseif ($totalDevices >= 24)
+                    <img src="{{ asset('Images/Wireless/comm_slash1.gif') }}">
+                @else
+                @endif
+            </td>
+            <td align="center">
+                @if ($totalDevices >= 25)
+                    <img src="{{ asset('Images/Wireless/comm_vert.gif') }}">
+                @else
+                @endif
+            </td>
+        </tr>
+        <tr>
+            <td width="60">{!! $deviceLayout[16] !!}</td>
+            <td width="60" align="center">
+                @if ($totalDevices >= 18)
+                    <img src="{{ asset('Images/Wireless/comm_horiz.gif') }}">
+                @else
+                @endif
+            </td>
+            <td width="60">{!! $deviceLayout[17] !!}</td>
+            <td width="60" align="center">
+                @if ($totalDevices >= 19)
+                    <img src="{{ asset('Images/Wireless/comm_horiz.gif') }}">
+                @else
+                @endif
+            </td>
+            <td width="60">{!! $deviceLayout[18] !!}</td>
+            <td width="60" align="center">
+                @if ($totalDevices >= 20)
+                    <img src="{{ asset('Images/Wireless/comm_horiz.gif') }}">
+                @else
+                @endif
+            </td>
+            <td width="60">{!! $deviceLayout[19] !!}</td>
+            <td width="60" align="center">
+                @if ($totalDevices >= 25)
+                    <img src="{{ asset('Images/Wireless/comm_horiz.gif') }}">
+                @else
+                @endif
+            </td>
+            <td width="60">{!! $deviceLayout[24] !!}</td>
+        </tr>
+        <tr>
+            <td align="center">
+                @if ($totalDevices >= 26)
+                    <img src="{{ asset('Images/Wireless/comm_vert.gif') }}">
+                @else
+                @endif
+            </td>
+            <td>
+                @if ($totalDevices >= 27)
+                    <img src="{{ asset('Images/Wireless/comm_slash2.gif') }}">
+                @elseif ($totalDevices >= 26)
+                    <img src="{{ asset('Images/Wireless/comm_slash1.gif') }}">
+                @else
+                @endif
+            </td>
+            <td align="center">
+                @if ($totalDevices >= 27)
+                    <img src="{{ asset('Images/Wireless/comm_vert.gif') }}">
+                @else
+                @endif
+            </td>
+            <td>
+                @if ($totalDevices >= 28)
+                    <img src="{{ asset('Images/Wireless/comm_slash2.gif') }}">
+                @elseif ($totalDevices >= 27)
+                    <img src="{{ asset('Images/Wireless/comm_slash1.gif') }}">
+                @else
+                @endif
+            </td>
+            <td align="center">
+                @if ($totalDevices >= 28)
+                    <img src="{{ asset('Images/Wireless/comm_vert.gif') }}">
+                @else
+                @endif
+            </td>
+            <td>
+                @if ($totalDevices >= 29)
+                    <img src="{{ asset('Images/Wireless/comm_slash2.gif') }}">
+                @elseif ($totalDevices >= 28)
+                    <img src="{{ asset('Images/Wireless/comm_slash1.gif') }}">
+                @else
+                @endif
+            </td>
+            <td align="center">
+                @if ($totalDevices >= 29)
+                    <img src="{{ asset('Images/Wireless/comm_vert.gif') }}">
+                @else
+                @endif
+            </td>
+            <td>
+                @if ($totalDevices >= 30)
+                    <img src="{{ asset('Images/Wireless/comm_slash2.gif') }}">
+                @elseif ($totalDevices >= 29)
+                    <img src="{{ asset('Images/Wireless/comm_slash1.gif') }}">
+                @else
+                @endif
+            </td>
+            <td align="center">
+                @if ($totalDevices >= 30)
+                    <img src="{{ asset('Images/Wireless/comm_vert.gif') }}">
+                @else
+                @endif
+            </td>
+        </tr>
+        <tr>
+            <td width="60">{!! $deviceLayout[25] !!}</td>
+            <td width="60" align="center">
+                @if ($totalDevices >= 27)
+                    <img src="{{ asset('Images/Wireless/comm_horiz.gif') }}">
+                @else
+                @endif
+            </td>
+            <td width="60">{!! $deviceLayout[26] !!}</td>
+            <td width="60" align="center">
+                @if ($totalDevices >= 28)
+                    <img src="{{ asset('Images/Wireless/comm_horiz.gif') }}">
+                @else
+                @endif
+            </td>
+            <td width="60">{!! $deviceLayout[27] !!}</td>
+            <td width="60" align="center">
+                @if ($totalDevices >= 29)
+                    <img src="{{ asset('Images/Wireless/comm_horiz.gif') }}">
+                @else
+                @endif
+            </td>
+            <td width="60">{!! $deviceLayout[28] !!}</td>
+            <td width="60" align="center">
+                @if ($totalDevices >= 30)
+                    <img src="{{ asset('Images/Wireless/comm_horiz.gif') }}">
+                @else
+                @endif
+            </td>
+            <td width="60">{!! $deviceLayout[29] !!}</td>
+        </tr>
